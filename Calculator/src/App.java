ef06cc2e519baef107c97f8da621fa55ccd2bd96
Lawrence Dacut <lawrencedacut20@gmail.com>
2023-08-21 20:16:17 +0800

    2

---
+++ Calculator/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+
+    static  int Add(int q, int y) {
+        return q + y;
+      }
+      static int Subtract(int q, int y) {
+        return q - y;
+      }
+      static int Multiply(int q, int y) {
+        return q * y;
+      }
+      static int Divide(int q, int y) {
+        return q / y;
+      }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
